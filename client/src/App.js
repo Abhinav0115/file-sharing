e064d7b326e5572359c2dc618fff71bb1b1af6ad
Abhinav Anand <abhinav011501@gmail.com>
2024-02-14 02:44:36 +0530

    Update: UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Left from "./components/Left_Side";
 
+import Left from "./components/Left_Side";
 import Right from "./components/Right_side";
 
 function App() {
     return (
-        <div className="bg-gradient-to-r from-slate-700 to-neutral-600 flex max-md:flex-col h-[100vh] w-[100vw] overflow-hidden object-cover">
+        <div className="bg-gradient-to-r from-slate-700 to-neutral-600 flex max-md:flex-col h-[100vh] w-[100vw] overflow-hidden">
             <Left />
             <Right />
         </div>
--- client/src/components/Left_Side.js
+++ client/src/components/Left_Side.js
@@ -1,13 +1,11 @@
 import React from "react";
-import share2 from "../Image/share2.jpg";
+import share from "../Image/share2.jpg";
 
 const Left = () => {
-    //"https://wallpapers.com/images/hd/road-background-r2y90ys6nzq1wu0w.jpg",
-
     return (
         <img
             className=" w-[100%] md:w-[30%] h-[50%] md:h-[100%] object-fill"
-            src={share2}
+            src={share}
             alt="banner"
         />
     );
